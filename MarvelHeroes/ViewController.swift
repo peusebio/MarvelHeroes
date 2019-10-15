28dbe87a3cac351c9351a3e5e7d96f853ff45783
Pedro Eusébio <1140432@isep.ipp.pt>
2019-10-15 22:03:45 +0100

    Initalizes the MarvelHeroes project

---
+++ MarvelHeroes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelHeroes
+//
+//  Created by Pedro Eusébio on 15/10/2019.
+//  Copyright © 2019 Pedro Eusébio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
